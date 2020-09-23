da2397f036804fe811360a0e5bf5be9b6e10160c
Коротенко Артур <akorotenko@rnds.pro>
2020-09-24 01:14:33 +0300

    Add devise

--- Gemfile
+++ Gemfile
@@ -54,4 +54,5 @@ end
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 gem 'cable_ready'
+gem 'devise'
 gem 'slim'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   validates :phone, format: { with: /\A(\+?7?|8)(\(?9\d{2}\)?\d{7})\z/,
                               message: 'Неправильный формат номера телефона' },
                     uniqueness: { message: 'Этот номер уже используется' }
-
 end
